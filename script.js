ea780b8227ce388528a20e1c278205904f6d30eb
raifescomputer <raifescomputer@gmail.com>
2025-02-27 11:18:48 -0500

    added javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("h2").textContent = "Welcome to Raife's Computer Blog!";
+
+document.querySelector("img").addEventListener("click", function() {
+    alert("You clicked the computer image!");
+});
+
+console.log("JavaScript is working!");
